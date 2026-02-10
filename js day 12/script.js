91f503ba47054f32995744a7f97d5a83e603b055
anshikarohilla6 <anshikarohilla6@gmail.com>
2026-02-10 12:07:29 +0530

    day12

---
+++ js day 12/script.js
@@ -0,0 +1,10 @@
+// const nameinput = document.getElementById("myinput");
+// nameinput.addEventListener("change",()=>{
+//     document.getElementById("output").textContent="changedetected!";
+//     console.log(nameinput.value);
+// })
+
+const nameinput = document.getElementById("myinput");
+const courseinput=document.getElementById("course");
+const btn=document.getElementById("btn");
+const output=document.getElementById("output");
