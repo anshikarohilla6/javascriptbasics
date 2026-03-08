34589b4f14ac1e87e8099f1c7e43deb09eea461d
anshikarohilla6 <anshikarohilla6@gmail.com>
2026-03-08 09:04:30 +0530

    asynchronous function

--- day16.js/script.js
+++ day16.js/script.js
@@ -1,21 +1,46 @@
-const pro = new Promise((res,rej)=>{
-    setTimeout(()=> {
-        let payment = false;
-        if (payment==="true")
-            res("payment done");
-        else
-            rej("payment failed")
-    },3000)
-})
+// const pro = new Promise((res,rej)=>{
+//     setTimeout(()=> {
+//         let payment = false;
+//         if (payment==="true")
+//             res("payment done");
+//         else
+//             rej("payment failed")
+//     },3000)
+// })
 
-async function getData () {
-    try{
-        const value = await pro;
-        console.log(value);
-        console.log("dashboard access given");
-    }catch(error){
-        console.error(error);
-        console.log("access denied");
-    }
-}
-getData();
+// async function getData () {
+//     try{
+//         const value = await pro;
+//         console.log(value);
+//         console.log("dashboard access given");
+//     }catch(error){
+//         console.error(error);
+//         console.log("access denied");
+//     }
+// }
+// getData();
+
+// async function fetchData(){
+//     try{
+//         const respons = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         return data;
+//     }catch (error){
+//         console.error(error);
+//     }
+// }
+
+// fetchData()
+// .then(data=>console.log(data))
+
+// async function  getData() {
+//     const result = await fetchData();
+//     console.log(result)
+// }
+// getData()
+
+
+
+
+
+
